f8943a21a789a23bb5e02185bdb7aca54b415fc8
Nato <1900011764@pku.edu.cn>
2022-06-09 23:29:08 +0800

    shell cmd (default experiment = 0)

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
-python main.py --dataset cora --seed 42 --epoch 50 --lr 1e-2 --wd 5e-4 --hidden_dim 64 --dp 0.5
-python main.py --dataset texas --seed 42 --epoch 100 --lr 2e-2 --wd 5e-4 --hidden_dim 64 --dp 0.5
-python main.py --dataset chameleon --seed 42 --epoch 200 --lr 1e-2 --wd 5e-4 --hidden_dim 64 --dp 0.5
+python main.py --dataset cora --seed 42 --epoch 100 --lr 0.1 --wd 5e-3 --hidden_dim 64 --dp 0.5 --experiment 0
+python main.py --dataset texas --seed 42 --epoch 100 --lr 0.05 --wd 1e-2 --hidden_dim 64 --dp 0.6 --experiment 0
+python main.py --dataset chameleon --seed 42 --epoch 300 --lr 0.01 --wd 5e-5 --hidden_dim 64 --dp 0.8 --experiment 0
